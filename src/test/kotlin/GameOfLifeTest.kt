76fc0dae140a5eefd5f8b6677aad07272d385f57
Fabien Giannesini <fabien.giannesini@gmail.com>
2022-08-27 16:44:53 +0200

    should handle all rules at the same time

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -82,4 +82,28 @@ class GameOfLifeTest {
         assertFalse(newCells[1])
         assertFalse(newCells[4])
     }
+
+    @Test
+    fun `should handle all rules at the same time`() {
+        // 0 0 1
+        // 1 1 0
+        // 1 1 0
+        val cells = BooleanArray(9)
+        cells[7] = true
+        cells[6] = true
+        cells[4] = true
+        cells[3] = true
+        cells[2] = true
+        val newCells = GameOfLife().run(cells)
+        // 0 1 0
+        // 1 0 1
+        // 1 1 0
+        val expected = BooleanArray(9)
+        expected[7] = true
+        expected[6] = true
+        expected[5] = true
+        expected[3] = true
+        expected[1] = true
+        assertArrayEquals(expected, newCells)
+    }
 }
