6a9f9b967195fb8fe497432d1755b5589011dcac
Fabien Giannesini <fabien.giannesini@gmail.com>
2022-08-27 10:31:07 +0200

    Init project

---
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class GameOfLifeTest {
+
+    @Test
+    internal fun `should do something`() {
+        assertTrue(true)
+    }
+}
