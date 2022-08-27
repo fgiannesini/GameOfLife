57f17400a530057d1ce782bbae31a492e795bece
Fabien Giannesini <fabien.giannesini@gmail.com>
2022-08-27 22:40:41 +0200

    Some tuning on the UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
+import java.awt.Color
+import java.awt.Dimension
 import java.awt.EventQueue
-
+import java.awt.Font
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import javax.swing.JFrame
+import javax.swing.JLabel
 import javax.swing.JTable
 import javax.swing.Timer
+import javax.swing.table.DefaultTableCellRenderer
+
 
 class SimpleEx(title: String) : JFrame() {
 
@@ -12,25 +19,47 @@ class SimpleEx(title: String) : JFrame() {
 
     private fun createUI(title: String) {
 
-        setTitle(title)
-
-        defaultCloseOperation = EXIT_ON_CLOSE
-        setSize(400, 300)
-        setLocationRelativeTo(null)
-
-
         val pattern = World(3, 1, booleanArrayOf(true, true, true))
         var world = World(5, 5)
             .addPatternAt(1, 2, pattern)
 
         val gameOfLife = GameOfLife()
 
+        setSize(400, 400)
+        setTitle(title)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        setLocationRelativeTo(null)
+
         // Initializing the JTable
         val table = JTable(Array(world.height) { Array(world.width) { "" } }, Array(world.width) { "" })
-
+        table.gridColor = Color(240, 240, 240)
+        table.font = Font("Serif", Font.BOLD, 20)
         updateTable(table, world)
+        table.addComponentListener(object : ComponentAdapter() {
+            override fun componentResized(e: ComponentEvent?) {
+                //Get new JTable component size
+                val size: Dimension = size
 
-        table.setBounds(30, 40, 200, 300)
+                //Check if height or width is the limiting factor and set cell size accordingly
+                val cellSize: Int = if (size.height / world.height > size.width / world.width) {
+                    size.width / world.width
+                } else {
+                    size.height / world.height
+                }
+
+                //Set new row height to our new size
+                table.rowHeight = cellSize
+
+                val centerRenderer = DefaultTableCellRenderer()
+                centerRenderer.horizontalAlignment = JLabel.CENTER
+                //Set new column width to our new size
+                for (i in 0 until table.columnCount) {
+                    val column = table.columnModel.getColumn(i)
+                    column.maxWidth = cellSize
+                    column.cellRenderer = centerRenderer
+                }
+            }
+        })
         add(table)
 
         val timer = Timer(1000) {
